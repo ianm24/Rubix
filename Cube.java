6cef65368de5bccb5628265a9a53960d21cbf829
ianm24 <ianmcdowell24@gmail.com>
2020-05-30 13:36:01 -0400

    Added JavaDoc for Cube class

--- Cube.java
+++ Cube.java
@@ -7,6 +7,9 @@ public class Cube {
 	private Face[] faces; //0:Top; 1:Bottom; 2:North; 3:East; 4:South; 5:West
 	private static final String[] FACE_ORDER = {"Top", "Bottom", "North", "East", "South", "West"};
 	
+	/**
+	 * Default constructor for Cube. Makes a 2x2x2 Cube
+	 */
 	public Cube() {
 		Face[] faces = new Face[6];
 		for(int i = 0; i < 6; i++) {
@@ -15,6 +18,10 @@ public class Cube {
 		this.faces = faces;
 	}
 	
+	/**
+	 * Constructor that makes a Cube with specific size 
+	 * @param size The size of each face [row,col]
+	 */
 	public Cube(int[] size) {
 		Face[] faces = new Face[6];
 		for(int i = 0; i < 6; i++) {
@@ -23,6 +30,10 @@ public class Cube {
 		this.faces = faces;
 	}
 	
+	/**
+	 * Constructor that makes a Cube with custom Faces
+	 * @param faces The 6 faces of the cube
+	 */
 	public Cube(Face[] faces) {
 		try {
 			this.setFace(faces);
@@ -31,6 +42,11 @@ public class Cube {
 		}
 	}
 	
+	/**
+	 * Used to validate and set Cube faces
+	 * @param faces The faces wished to be set
+	 * @throws InvalidNumOfFacesException if there aren't 6 faces
+	 */
 	private void setFace(Face[] faces) throws Exception{
 		if(faces.length == 6) {
 			this.faces = faces;
@@ -39,6 +55,9 @@ public class Cube {
 		}
 	}
 	
+	/**
+	 * Returns information about Cube as string
+	 */
 	public String toString() {
 		String retStr = new String();
 		for(int i = 0; i < this.faces.length; i++) {
